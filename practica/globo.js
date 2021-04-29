f5bc8d5ad1c52acbb302db0ba9e9257881ce045d
brayan <tipan88@gmail.com>
2021-04-29 23:49:44 +0200

    Globos casi terminados

---
+++ practica/globo.js
@@ -0,0 +1,81 @@
+class Globo {
+    constructor(gravity) {
+        this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        this.posX = Math.floor(Math.random() * 761) + 20;
+        this.posY = 445;
+        this.width1 = this.posX - 18;
+        this.height1 = this.posY - 28;
+        this.width2 = 36;
+        this.height2 = 56;
+        this.gravity = gravity;
+        this.gravitySpeed = 0;
+        this.estado = 'active';
+        this.contexto = document.querySelector('canvas').getContext("2d");
+    }
+
+    set changeGravity(gravity) {
+        this.gravity += gravity;
+    }
+
+    get verEstado() {
+        return this.estado;
+    }
+
+    get verPosy() {
+        return this.posY;
+    }
+
+    draw() {
+        this.contexto.beginPath();
+        this.contexto.fillStyle = this.color;
+        this.contexto.ellipse(this.posX, this.posY, 18, 28, 0, 0, 2 * Math.PI, true);        
+        this.contexto.fill();
+        this.contexto.closePath();
+        
+        this.contexto.beginPath();
+        this.contexto.lineWidth = 2;
+        this.contexto.moveTo(this.posX, this.posY + 28);
+        this.contexto.lineTo(this.posX, this.posY + 60);        
+        this.contexto.stroke();
+        this.contexto.closePath();        
+    }
+
+    move() {
+        if (this.estado == 'active') {
+            this.contexto.clearRect(this.width1, this.height1, this.width2, this.height2 + 35);
+            this.gravitySpeed += this.gravity;
+            this.posY -= this.gravitySpeed;
+            this.height1 = this.posY - 28;
+            this.draw();
+        }
+    }
+
+    burstBallon(posX, posY) {
+        if ((posX >= this.width1 && posX <= (this.width1 + this.width2)) && (posY >= this.height1 && posY <= (this.height1 + this.height2))) {
+            let posX = this.posX;
+            let posY = this.posY;
+            let dimension = 40;
+            let lad = 5;
+            let star = lad / 2;
+            let rad = (2 * Math.PI) / star;
+            this.estado = 'exploited';
+            this.contexto.clearRect(this.width1, this.height1, this.width2, this.height2);
+            this.contexto.fillStyle = this.color;
+            this.contexto.lineWidth = 3;
+            this.contexto.beginPath();
+            for (var i = 0; i < lad; i++) {
+                let x = posX + dimension * Math.cos(rad * i);
+                let y = posY + dimension * Math.sin(rad * i);
+                this.contexto.lineTo(x, y);
+            }
+            this.contexto.closePath();
+            this.contexto.stroke();
+            this.contexto.fill();
+            setTimeout(function (globo) {
+                globo.contexto.clearRect(globo.width1 - 25, globo.height1 - 20, globo.width2 + 56, globo.height2 + 65);
+            }, 100, this);
+            return true;
+        }
+    }
+
+}
---
+++ practica/juego.js
@@ -0,0 +1,121 @@
+class gameArea {
+
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = '800';
+        this.canvas.height = '570';
+        this.canvas.style.border = '1px black solid';
+        this.panel = document.createElement('canvas');
+        this.contextoPanel = this.panel.getContext('2d');
+        this.panel.width = '800';
+        this.panel.height = '50';
+        this.panel.style.border = '1px black solid';
+        document.body.appendChild(this.canvas);
+        document.body.appendChild(this.panel);
+    }
+
+    start() {
+        let contexto = this.canvas.getContext('2d');
+        let backgr = new Image();
+        backgr.src = './cesped.png';        
+        backgr.onload = function () {
+            contexto.drawImage(backgr, 0, 500);
+            contexto.globalCompositeOperation='destination-over'
+        }        
+        this.ballons = [];
+        this.points = 0;
+        this.minuts = 3;
+        this.speed = 0.8;
+        this.seconds = 60;
+        this.status = 'start';
+        this.speedMake = 700;
+        this.make = setInterval(this.makeBallon, this.speedMake, this);
+        this.move = setInterval(this.moveBallons, 100, this);
+        this.contextoPanel.beginPath();
+        this.contextoPanel.font = "bold 30px verdana";
+        this.contextoPanel.fillText('Score: ' + this.points, 5, 30);
+        this.contextoPanel.font = "bold 15px arial";
+        this.contextoPanel.fillText('Key <A>: speed     Key <space>: pause/start', 225, 30);
+        this.contextoPanel.font = "bold 25px verdana";
+        this.contextoPanel.fillText('Time = ' + this.minuts + ':' + this.seconds, 640, 30);
+        this.contextoPanel.closePath();
+        this.countTime = setInterval(this.printTime, 1000, this);
+    }
+
+    get viewStatus() {
+        return this.status;
+    }
+    printTime(area) {
+        area.seconds--;
+        if (area.seconds < 0) {
+            area.minuts--;
+            area.seconds = 60;
+        }
+        area.contextoPanel.clearRect(640, 0, 200, 50);
+        area.contextoPanel.beginPath();
+        area.contextoPanel.font = "bold 25px verdana";
+        if (area.seconds < 10) {
+            area.contextoPanel.fillText('Time = ' + area.minuts + ':0' + area.seconds, 640, 30);
+        } else {
+            area.contextoPanel.fillText('Time = ' + area.minuts + ':' + area.seconds, 640, 30);
+        }
+        area.contextoPanel.closePath();
+    }
+
+    stop() {
+        this.status = 'stop';
+        clearInterval(this.make);
+        clearInterval(this.move);
+        clearInterval(this.countTime);
+    }
+
+    restart() {
+        this.status = 'start';
+        this.make = setInterval(this.makeBallon, this.speedMake, this);
+        this.move = setInterval(this.moveBallons, 100, this);
+        this.countTime = setInterval(this.printTime, 1000, this);
+    }
+
+    makeBallon(area) {
+        let ballon = new Globo(area.speed);
+        ballon.draw();
+        area.ballons.push(ballon);
+    }
+
+    moveBallons(area) {
+        for (let i = 0; i < area.ballons.length; i++) {
+            area.ballons[i].move();
+        }
+    }
+
+    removeBallons(event) {
+        for (let i = 0; i < this.ballons.length; i++) {
+            if (this.ballons[i].verEstado == 'active') {
+                if (this.ballons[i].burstBallon(event.pageX, event.pageY)) {
+                    this.points++;
+                    this.contextoPanel.clearRect(0, 0, 200, 50);
+                    this.contextoPanel.beginPath();
+                    this.contextoPanel.font = "bold 30px verdana";
+                    this.contextoPanel.fillText('Score: ' + this.points, 5, 30);
+                    this.contextoPanel.closePath();
+                }
+            }
+        }
+    }
+
+    deleteBallonArray(ballon) {
+        let posBallon = this.ballons.indexOf(ballon);
+        if (posBallon != -1) {
+            this.ballons.splice(posBallon, posBallon + 1);
+        }
+    }
+
+    increaseDifficulty() {
+        if (this.speed < 2.5) {
+            this.speed += 0.2;
+            for (let i = 0; i < this.ballons.length; i++) {
+                this.ballons[i].changeGravity = this.speed;
+            }
+        }
+    }
+}
